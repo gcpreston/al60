7d2362f8cf629ddb1fe925aa1f69b8d791be120a
Graham Preston <preston.gr@husky.neu.edu>
2018-12-12 02:18:34 -0500

    Change Graph to use adjacency list and implement pre/post-visit capable DFS method

--- data/graphs.py
+++ data/graphs.py
@@ -1,33 +1,106 @@
-class Graph:
+class DirectedGraph:
     """
-    An unweighted, undirected graph connecting nodes with edges.
+    An unweighted, directed graph connecting nodes with edges. Implemented using
+    an adjacency set for edge representation.
     """
 
     def __init__(self):
         """
         Initialize a new empty Graph instance.
         """
+        # TODO: Consider adjacency list vs. adjacency set
         self._nodes = set()
-        self._edges = set()
+        self._a_in = dict()
+        self._a_out = dict()
 
     def add_node(self, name):
         """
         Add a node to this graph.
 
         :param name: the name of the node to add
+        :raises ValueError if name is a previously defined node
         """
+        if name in self._nodes:
+            raise ValueError(f'node {name} is already defined')
+
         self._nodes.add(name)
+        self._a_in[name] = set()
+        self._a_out[name] = set()
 
     def add_edge(self, u, v):
         """
-        Add an edge between nodes u and v. Raises ValueError if u or v is not a defined node.
+        Add an edge between nodes u and v.
 
         :param u: the 'from' node
         :param v: the 'to' node
+        :raises ValueError if u or v is not a defined node or (u, v) is a
+            previously defined edge
         """
-        if not (u in self._nodes and v in self._nodes):
-            s1 = f"'{u}'" if isinstance(u, str) else u
-            s2 = f"'{v}'" if isinstance(v, str) else v
-            raise ValueError(f'invalid edge ({s1}, {s2})')
+        # add quotes to str if needed
+        su = f"'{u}'" if isinstance(u, str) else u
+        sv = f"'{v}'" if isinstance(v, str) else v
 
-        self._edges.add((u, v))
+        if not (u in self._nodes and v in self._nodes):
+            raise ValueError(f'invalid edge ({su}, {sv})')
+        if v in self._a_out[u]:
+            raise ValueError(f'edge ({su}, {sv}) is already defined')
+
+        self._a_in[v].add(u)
+        self._a_out[u].add(v)
+
+    def dfs(self, v, pre_visit=lambda *args: None,
+            post_visit=lambda *args: None):
+        """
+        Perform a depth-first search on this graph from node v.
+
+        :param v: the starting node
+        :param pre_visit: a function to execute on each discovered node before
+            it is visited
+        :param post_visit: a function to execute on each discovered node after
+            it is visited
+        :return: a list of the discovered nodes
+        :raises ValueError if v is not a defined node
+        """
+        if v not in self._nodes:
+            raise ValueError(f'node {v} is not defined')
+
+        found = set()
+
+        def _dfs(_u):
+            found.add(_u)
+
+            pre_visit(_u)
+            for _v in self._a_out[_u]:
+                if _v not in found:
+                    _dfs(_v)
+            post_visit(_u)
+
+        _dfs(v)
+        return found
+
+    def post_order(self, v):
+        """
+        Compute the post-order of this graph using a depth-first search from v.
+
+        :param v: the node to search from
+        :return: a list of nodes in the order they were done being processed
+        :raises ValueError if v is not a defined node
+        """
+        if v not in self._nodes:
+            raise ValueError(f'node {v} is not defined')
+
+        order = []
+        self.dfs(v, post_visit=lambda x: order.append(x))
+
+        return order
+
+    def topological_sort(self):
+        """
+        Topologically sort this graph using DFS calls from each node.
+
+        :return: a topological ordering of this graph
+        """
+        # TODO: Raise exception if not DAG, or make method on DAG type
+        # TODO: Implement using DFS
+
+        stack = []
--- test/test_data/test_graphs.py
+++ test/test_data/test_graphs.py
@@ -1,33 +1,87 @@
 import unittest
 
-from data.graphs import Graph
+from data.graphs import DirectedGraph
 
 
 class TestGraph(unittest.TestCase):
     """
-    Tests for data.graphs.Graph object.
+    Tests for data.graphs.DirectedGraph object.
     """
 
     def setUp(self):
-        self.g1 = Graph()
+        self.g_empty = DirectedGraph()
+
+        self.g1 = DirectedGraph()
+        self.g1.add_node('u')
+        self.g1.add_node('a')
+        self.g1.add_node('b')
+        self.g1.add_node('c')
+        self.g1.add_node('x')
+        self.g1.add_node('y')
+
+        self.g1.add_edge('u', 'a')
+        self.g1.add_edge('a', 'u')
+        self.g1.add_edge('u', 'c')
+        self.g1.add_edge('c', 'a')
+        self.g1.add_edge('c', 'b')
+        self.g1.add_edge('b', 'u')
+        self.g1.add_edge('x', 'y')
+
+        self.g2 = DirectedGraph()
+        self.g2.add_node('a')
+        self.g2.add_node('b')
+        self.g2.add_node('c')
+        self.g2.add_node('d')
+
+        self.g2.add_edge('a', 'b')
+        self.g2.add_edge('a', 'd')
+        self.g2.add_edge('b', 'd')
+        self.g2.add_edge('c', 'd')
 
     def test_add_node(self):
-        self.g1.add_node(1)
-        self.g1.add_node(1)
-        self.g1.add_node(2)
-        self.g1.add_node('hello')
+        self.g_empty.add_node(1)
+        self.g_empty.add_node('hello')
+        # NOTE: When True is added, a ValueError is raised???
+        self.g_empty.add_node(False)
 
-        self.assertEqual(self.g1._nodes, {1, 2, 'hello'})
+        self.assertEqual({1, 'hello', False}, self.g_empty._nodes)
+
+        self.assertRaises(ValueError, self.g_empty.add_node, 1)
 
     def test_add_edge(self):
-        self.g1.add_node(1)
-        self.g1.add_node(2)
-        self.g1.add_edge(1, 2)
+        self.assertRaises(ValueError, self.g_empty.add_edge, 'fake1', 'fake2')
 
-        self.assertEqual(self.g1._edges, {(1, 2)})
+        self.g_empty.add_node(1)
+        self.g_empty.add_node(2)
+        self.g_empty.add_edge(1, 2)
+
+        self.assertEqual(self.g_empty._a_in, {1: set(), 2: {1}})
+        self.assertEqual(self.g_empty._a_out, {1: {2}, 2: set()})
+
+        self.assertRaises(ValueError, self.g_empty.add_edge, 1, 2)
 
     def test_add_edge_undefined_node(self):
-        self.assertRaises(ValueError, self.g1.add_edge, 'hello', 'world')
+        self.assertRaises(ValueError, self.g_empty.add_edge, 'hello', 'world')
+
+    def test_dfs(self):
+        self.assertRaises(ValueError, self.g1.dfs, 'fake')
+
+        u_found = self.g1.dfs('u')
+        x_found = self.g1.dfs('x')
+
+        self.assertEqual({'u', 'a', 'c', 'b'}, u_found)
+        self.assertEqual({'x', 'y'}, x_found)
+
+        # TODO: Test pre_visit and post_visit
+
+    def test_post_order(self):
+        self.assertRaises(ValueError, self.g1.post_order, 'fake')
+
+        self.assertEqual(['a', 'b', 'c', 'u'], self.g1.post_order('u'))
+        self.assertEqual(['y', 'x'], self.g1.post_order('x'))
+
+    def test_topological_sort(self):
+        print(self.g2.topological_sort())
 
 
 if __name__ == '__main__':
