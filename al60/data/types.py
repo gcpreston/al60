fcb29c72c86361097649a36919dd767d76d4c649
Graham Preston <preston.gr@husky.neu.edu>
2018-12-25 21:17:35 -0500

    Documentation

--- al60/data/types.py
+++ al60/data/types.py
@@ -1,4 +1,10 @@
 from typing import Hashable, Tuple
 
+# Ideally, a Node would be defined as the name of a node in a specified graph.
+# However, since Python's type hints are static, this type of definition is not
+# possible. Instead, this idea is simulated by defining a Node and an Edge as
+# any possible node or edge in some graph, and it is left up to each method
+# to check if the Node or Edge is valid for the relevant graph.
+
 Node = Hashable
 Edge = Tuple[Node, Node]
